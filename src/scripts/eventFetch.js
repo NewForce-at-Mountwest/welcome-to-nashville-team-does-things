0bd775c0781fd16e65c6dc57d3e8b33191340ffe
Newforce <thecreativefitz@gmail.com>
2019-02-07 22:45:52 -0500

    established tokens within api

--- src/scripts/eventFetch.js
+++ src/scripts/eventFetch.js
@@ -1,2 +1,10 @@
-allIds = document.querySelectorAll('*[id]');
-console.log(allIds);
+fetch("https://www.eventbriteapi.com/v3/events/search/?q=nashville_coding&token=YNYWXAM4XRQXRPS65ZCV", {
+ headers: {
+   "Authorization": "Bearer YNYWXAM4XRQXRPS65ZCV",
+   "Accept": "application/json"
+ }
+})
+.then(events => events.json())
+.then(results =>{
+    console.log(results)
+})
